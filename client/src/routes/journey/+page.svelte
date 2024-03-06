5a4dd6c56a080329610c2419bcd749e04db79ee9
rodrq <rodrqq@gmail.com>
2024-03-06 16:12:55 -0300

    WIP setting up quest view per id

--- client/src/routes/journey/+page.svelte
+++ client/src/routes/journey/+page.svelte
@@ -6,12 +6,14 @@
 <head>
     <title>RPG Quest - Journey</title>
 </head>
-  
-<div>
-    <h1>Quests</h1>
+
+<div class="container mx-auto px-10 text-gray-600 text-xl">
+    <h1 class="text-2xl font-bold mb-4">{quests[0].character_username} journey</h1>
     <div>
-        {#each quests as quest (quest.id)}
-            <p>{quest.title}</p>
+        {#each quests as quest (quest.quest_id)}
+            <a href="/journey" class="hover:text-red-500">
+                - {quest.title}
+            </a>
         {/each}
     </div>
 </div>
