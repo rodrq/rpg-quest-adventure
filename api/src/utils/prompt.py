bc6e33b1595a813c706471403e68a28f65f3a56e
rodrq <rodrqq@gmail.com>
2024-03-06 19:43:02 -0300

    shrinked map size

--- api/src/utils/prompt.py
+++ api/src/utils/prompt.py
@@ -20,18 +20,15 @@ def create_quest_prompt(username: str, class_: str, map: str, virtue:str, flaw:s
                         The bolder the approach, the lesser the 'chance_of_success'"""
                     
     user_prompt = f""""Hello gamemaster, My name is {username} and I'm a {class_}. I'm currently in the {map}. 
-                        My virtue is {virtue}, and my flaw is {flaw}"""
+                        My virtue is {virtue}, and my flaw is {flaw}. Generate a fun quest for me using this context """
     return system_prompt, user_prompt 
 
 prompt_maps = {
-    1:'Farm village',
-    2:'River',
-    3:'Abandoned castle',
-    4:'Catacombs',
-    5:'Sewage',
-    6:'Plague-ridden town',
-    7:'Hills',
-    8:'Snowy mountain',
-    9:'Dwarf city',
-    10:"Dwarf king's hall. The last step before the end of my adventure.",
+    1:'village',
+    2:'abandoned castle',
+    3:'catacombs',
+    4:'hills',
+    5:'snowy mountain',
+    6:'dwarf city',
+    7:"dwarf king's hall. The last step before the end of my adventure.",
   }
