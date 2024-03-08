cf75224b103a395354137626c22d1f9b7bba2f33
rodrq <rodrqq@gmail.com>
2024-03-08 09:28:55 -0300

    moved admin dependencies from function to decorator to avoid "unused function" warning

--- api/src/routes/admin.py
+++ api/src/routes/admin.py
@@ -1,51 +1,45 @@
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends
+from fastapi.responses import JSONResponse
+from sqlalchemy.orm import Session
 from src.utils.auth import get_current_admin_user
 from src.config.database import get_db
 from src.models.models import Character, Quest
-from sqlalchemy.orm import Session
+
 
 router = APIRouter(prefix='/admin',
                    tags=['Admin'])
 
-@router.get("/get_quests")
-async def admin_resource(
-    current_admin_user: Character = Depends(get_current_admin_user),
-    db: Session = Depends(get_db)):
+
+@router.get("/get_quests", dependencies=[Depends(get_current_admin_user)])
+async def get_quests(db: Session = Depends(get_db)):
     quests = db.query(Quest).all()
     return quests
 
 
-@router.get("/get_quest/{quest_id}")
-async def get_quest(quest_id: int, 
-                    current_admin_user: Character = Depends(get_current_admin_user),
-                    db: Session = Depends(get_db)):
+@router.get("/get_quest/{quest_id}", dependencies=[Depends(get_current_admin_user)])
+async def get_quest(quest_id: int, db: Session = Depends(get_db)):
     quest = db.query(Quest).filter(Quest.quest_id == quest_id).first()
-    if not quest:
-        raise HTTPException(status_code=401, detail="Invalid credentials")
     return quest
-    
-@router.get('/get_character/{character_name}')
-async def get_character(character_name: str,
-                        current_admin_user: Character = Depends(get_current_admin_user),
-                        db: Session = Depends(get_db)):
-    character = db.query(Character).filter(Character.username == character_name).first()
+
+
+@router.get("/get_character/{character_name}", dependencies=[Depends(get_current_admin_user)])
+async def get_character(character_name: str, db: Session = Depends(get_db)):
+    character = db.query(Character).filter(
+        Character.username == character_name).first()
     return character
 
-@router.get('/get_characters')
-async def get_characters(current_admin_user: Character = Depends(get_current_admin_user), 
-                         db: Session = Depends(get_db)):
-    
+
+@router.get("/get_characters", dependencies=[Depends(get_current_admin_user)])
+async def get_characters(db: Session = Depends(get_db)):
     characters = db.query(Character).all()
     return characters
 
-@router.delete('/delete_character')
-async def delete_character(username: str,
-                           db: Session = Depends(get_db),
-                           current_admin_user: Character = Depends(get_current_admin_user)):
-    character = db.query(Character).filter(Character.username == username).first()
+
+@router.delete('/delete_character', dependencies=[Depends(get_current_admin_user)])
+async def delete_character(username: str, db: Session = Depends(get_db)):
+    character = db.query(Character).filter(
+        Character.username == username).first()
     if character:
         db.delete(character)
         db.commit()
-        return {"message": "Character deleted successfully"}
-    else:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Character not found")
+        return JSONResponse(content="Character deleted")
