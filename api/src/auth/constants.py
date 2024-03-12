c4dbcdf9ade0db6a207a4c2d408f45697d9c0e09
rodrq <rodrqq@gmail.com>
2024-03-12 01:04:44 -0300

    moved errorcode constants to be src/constants because they are global

--- api/src/auth/constants.py
+++
@@ -1,9 +0,0 @@
-class ErrorCode:
-    AUTHENTICATION_REQUIRED = "Authentication required."
-    AUTHORIZATION_FAILED = "Authorization failed. User has no access."
-    INVALID_TOKEN = "Invalid token."
-    INVALID_CREDENTIALS = "Invalid credentials."
-    FIELDS_ARE_EMPTY = "One or all required fields are empty"
-    USERNAME_TAKEN = "Username is already taken."
-    REFRESH_TOKEN_NOT_VALID = "Refresh token is not valid."
-    REFRESH_TOKEN_REQUIRED = "Refresh token is required either in the body or cookie."
--- api/src/constants.py
+++ api/src/constants.py
@@ -9,9 +9,20 @@ POSTGRES_INDEXES_NAMING_CONVENTION = {
     "pk": "%(table_name)s_pkey",
 }
 
-
-
 class Environment(str, Enum):
     LOCAL = "LOCAL"
     TESTING = "TESTING"
     PRODUCTION = "PRODUCTION"
+
+class ErrorCode:
+    AUTHENTICATION_REQUIRED = "Authentication required."
+    AUTHORIZATION_FAILED = "Authorization failed. User has no access."
+    INVALID_TOKEN = "Invalid token."
+    INVALID_CREDENTIALS = "Invalid credentials."
+    FIELDS_ARE_EMPTY = "One or all required fields are empty"
+    USERNAME_TAKEN = "Username is already taken."
+    CHARACTER_NAME_TAKEN = "Character name is already taken."
+    REFRESH_TOKEN_NOT_VALID = "Refresh token is not valid."
+    REFRESH_TOKEN_REQUIRED = "Refresh token is required either in the body or cookie."
+    CHARACTER_CANT_BE_DELETED = "Character either doesn't exist or isn't yours"
+    CHARACTER_NOT_YOURS = "Character isn't yours"
