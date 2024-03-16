ff9ead2679728b25dc34e0813ada209535f88335
rodrq <rodrqq@gmail.com>
2024-03-16 11:55:54 -0300

    re did the bug fix to a simpler solution

--- api/src/quest/game.py
+++ api/src/quest/game.py
@@ -39,7 +39,7 @@ async def success_roll(
     }
 
     # check if next map_level greater than max numbers of maps
-    if character.map_level + 1 >= len(llm.prompt_maps_dict + 1):
+    if character.map_level + 1 > (len(llm.prompt_maps_dict)):
         character_updates["state"] = "winner"
 
     quest_updates = {
